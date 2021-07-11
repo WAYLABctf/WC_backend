e7f300facb4eb8c908dc2979235e6d9cc14fc9b2
omh02033 <omh02033@gmail.com>
2021-07-11 22:25:49 +0900

    add: middleware

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const path = require("path");
+require("dotenv").config({
+    path: path.resolve(__dirname, "config", process.env.NODE_ENV == "production" ? ".env" : ".env.dev")
+});
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require('cookie-parser');
+const cors = require("cors");
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(cookieParser());
+app.use(cors());
+
+app.use('/', require('./routes/index.js'));
+
+app.use((req, res) => { err404(req, res); });
+
+app.listen(3000, () => { console.log("Connected !") });
+
+function err404(req, res) { res.sendFile('404.html', { root: path.join(__dirname, '/public/html/err') }); }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
-var express=require('express');
-var router=express.Router();
+var express = require('express');
+var router = express.Router();
 
-router.get('/',function(req,res){
-    res.send({greeting:"welcome to waylab CTF"});
+router
+.get('/', (req,res) => {
+    res.send({ greeting:"welcome to waylab CTF" });
+})
 
-});
-module.exports=router;
+module.exports = router;
