57097cbcffb9959924053cd50714ff5cdb9aee5b
pocas <pocas.cyber@gmail.com>
2021-07-12 04:51:36 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,32 @@
 const express = require('express');
-const path = require("path");
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const dbConnection = require('./db-connection');
 const router = express();
- 
-router.use(cookieParser());
+const SECRET = 'test'; // 임시
 
 router.get('/', (req,res) => {
-    res.send({ greeting:"welcome to waylab CTF" });
+    res.render('index.ejs', {message: "Beta Test" });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login.ejs');
+})
+
+router.post('/login', async (req, res) => {
+    const { body } = req;
+    try {
+        const [data] = await dbConnection.execute("SELECT * FROM users where username = ? and password = ?", [body.username, body.password]);
+        if (data.length < 1) {
+            res.render('login.ejs', {error: '가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.'});
+        } else {
+            const token = await jwt.sign({ user: data.username}, SECRET, { expiresIn: '1h'});
+            res.cookie('token', token);
+            res.redirect("/");           
+        }
+    }catch{
+        res.status(500);
+        res.send('500 Server Error');
+    }
 });
 
 router.get('/signup', (req, res) => {
@@ -22,17 +39,21 @@ router.post('/signup', async (req, res) => {
         const [data] = await dbConnection.execute("SELECT * FROM users where username = ?", [body.username]);
         if (data.length >= 1) {
             res.render('signup.ejs', {error: '사용자가 존재함.'});
+        } else{       
+            const [data_] = await dbConnection.execute("INSERT INTO users (username, password, nickname, email) values (?, ?, ?, ?)", [body.username, body.password, body.nickname, body.email]);
+            if (data_.affectedRows !== 1) {
+                res.render('signup.ejs', {error: '가입 실패'});
+            } else{ res.redirect("/login"); }
         }
-        
-        const [data_] = await dbConnection.execute("INSERT INTO users (username, password, nickname, email) values (?, ?, ?, ?)", [body.username, body.password, body.nickname, body.email]);
-        if (data_.affectedRows !== 1) {
-            res.render('signup.ejs', {error: '가입 에러'});
-        }
-        res.render('login.ejs');
-    }catch(e){
-        console.log(e);
-        res.send(e);
+    }catch{
+        res.status(500);
+        res.send('500 Server Error');
     }
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie("token");
+    res.redirect("/");
 });
 
 module.exports = router;
