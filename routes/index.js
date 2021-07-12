4b6f875bacb45dd810937d516677525401225064
pocas <pocas.cyber@gmail.com>
2021-07-12 16:10:32 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const crypto = require("crypto");
 const jwt = require('jsonwebtoken');
+const { uuid } = require('uuidv4');
+const { MailSend } = require('./Email-Send.js');
 const dbConnection = require('./db-connection');
 const router = express();
 const SECRET = 'test'; // 임시
@@ -38,15 +40,22 @@ router.get('/signup', (req, res) => {
 router.post('/signup', async (req, res) => {
     const { body } = req;
     try{
-        const [data] = await dbConnection.execute("SELECT * FROM users where username = ?", [body.username]);
+        const [data] = await dbConnection.execute("SELECT * FROM users where username = ? or email = ?", [body.username, body.email]);
         if (data.length >= 1) {
-            res.render('signup.ejs', {error: '사용자가 존재함.'});
+            res.render('signup.ejs', {error: '사용자 또는 이메일 중복'});
         } else{       
             const hash_pass = crypto.createHash("sha256").update(body.password, "binary").digest("hex");
-            const [data_] = await dbConnection.execute("INSERT INTO users (username, password, nickname, email) values (?, ?, ?, ?)", [body.username, hash_pass, body.nickname, body.email]);
+            const token = uuid();
+            const url = `http://localhost:3000/confirm/${token}`;
+            const [data_] = await dbConnection.execute("INSERT INTO users (username, password, nickname, email, email_verify, token) values (?, ?, ?, ?, ?, ?)", 
+                [body.username, hash_pass, body.nickname, body.email, false, token]
+            );
             if (data_.affectedRows !== 1) {
                 res.render('signup.ejs', {error: '가입 실패'});
-            } else{ res.redirect("/login"); }
+            } else{ 
+                MailSend(body.email, url);
+                res.send("Verification email sent!");
+            }
         }
     }catch (error){
         console.log(error);
@@ -54,6 +63,10 @@ router.post('/signup', async (req, res) => {
         res.send('500 Server Error');
     }
 });
+
+router.get('/confirm/:token', (req, res) => {
+    res.send(req.params.token);
+})
 
 router.get('/logout', (req, res) => {
     res.clearCookie("token");
