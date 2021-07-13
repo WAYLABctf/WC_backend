dffcdbf424e9caac5ebc3cebef4fb7a8b63f05e0
pocas <pocas.cyber@gmail.com>
2021-07-13 19:46:02 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,57 +6,68 @@ const { MailSend } = require('./Email-Send.js');
 const dbConnection = require('./db-connection');
 const router = express();
 const SECRET = 'test'; // 임시
+const eamil_regex  = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@dimigo.hs.kr$/i;  
 
 router.get('/', (req,res) => {
     res.render('index.ejs', {message: "Beta Test" });
 });
 
+// 로그인 페이지.
 router.get('/login', (req, res) => {
     res.render('login.ejs');
 })
 
+// 로그인 api 기능
 router.post('/login', async (req, res) => {
     const { body } = req;
     try {
         const hash_pass = crypto.createHash("sha256").update(body.password, "binary").digest("hex");
         const [data] = await dbConnection.execute("SELECT * FROM users where username = ? and password = ?", [body.username, hash_pass]);
-        if (data.length < 1) {
-            res.render('login.ejs', {error: '가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.'});
+        if (data.length < 1) { // 사용자 계정 존재 여부 검증.
+            res.render('login.ejs', {error: 'Not registered account or wrong password'});
         } else {
-            if (data[0]['email_verify'] == 1){
+            if (data[0]['email_verify'] == 1){ // 사용자 계정 인증 검증.
                 const token = await jwt.sign({ user: data[0]['username'], verify: data[0]['email_verify']}, SECRET, { expiresIn: '1h'});
                 res.cookie('token', token);
                 res.redirect("/");       
             } else { res.status(400); res.send("Please verify your email")}
         }
-    }catch (e) {
+    }catch (error) {
+        console.log(error);
         res.status(500);
         res.send('500 Server Error');
     }
 });
 
+// 회원 가입 페이지.
 router.get('/signup', (req, res) => {
     res.render('signup.ejs');
 });
 
+// 회원가입 api 기능
 router.post('/signup', async (req, res) => {
     const { body } = req;
     try{
         const [data] = await dbConnection.execute("SELECT * FROM users where username = ? or email = ?", [body.username, body.email]);
-        if (data.length >= 1) {
-            res.render('signup.ejs', {error: '사용자 또는 이메일 중복'});
-        } else{       
+        if (data.length >= 1) { // 아이디와 이메일 중복 여부 검증
+            res.render('signup.ejs', {error: 'Duplicate an username or email'});
+        } else{     
             const hash_pass = crypto.createHash("sha256").update(body.password, "binary").digest("hex");
-            const token = uuid();
-            const url = `http://141.164.47.126:3002/confirm/${token}`;
-            const [data_] = await dbConnection.execute("INSERT INTO users (username, password, nickname, email, email_verify, token) values (?, ?, ?, ?, ?, ?)", 
-                [body.username, hash_pass, body.nickname, body.email, false, token]
-            );
-            if (data_.affectedRows !== 1) {
-                res.render('signup.ejs', {error: '가입 실패'});
-            } else{ 
-                MailSend(body.email, url);
-                res.send("Verification email sent!");
+            if(!eamil_regex.test(body.email)) {
+                res.render('signup.ejs', {error: 'Only use a dimigo email.'});
+            }
+            else{
+                const token = uuid();
+                const url = `http://141.164.47.126:3002/confirm/${token}`;
+                const [data_] = await dbConnection.execute("INSERT INTO users (username, password, nickname, email, email_verify, token) values (?, ?, ?, ?, ?, ?)", 
+                    [body.username, hash_pass, body.nickname, body.email, false, token]
+                );
+                if (data_.affectedRows !== 1) {
+                    res.render('signup.ejs', {error: 'Register Failed'});
+                } else{ 
+                    MailSend(body.email, url);
+                    res.send("Verification email sent!");
+                }
             }
         }
     }catch (error){
@@ -66,13 +77,14 @@ router.post('/signup', async (req, res) => {
     }
 });
 
+// 이메일 인증 api 기능
 router.get('/confirm/:token', async (req, res) => {
     const token = req.params.token;
     try{
         const [data] = await dbConnection.execute("SELECT * FROM users where token = ?", [token]);
-        if (data.length >= 1) {
+        if (data.length >= 1) { // token을 가진 사용자 존재 여부 검증
             const [verify] = await dbConnection.execute("UPDATE users SET email_verify = 1, token = '' WHERE token = ?",[token]);
-            if (verify.info.indexOf("Changed: 1") != -1){
+            if (verify.info.indexOf("Changed: 1") != -1){ // 계정 활성화가 잘 되었는 지 검증.
                 res.redirect('/login');
             }
         } else {
