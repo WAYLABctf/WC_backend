cf614097f8208bcb85875e6d4f8a2ba66f33d82c
pocas <pocas.cyber@gmail.com>
2021-07-18 23:31:55 +0900

    Update db-connection.js

--- server/routes/db-connection.js
+++ server/routes/db-connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const dbConnection = mysql.createPool({
-    host: '141.164.47.126',
-    user: 'root',
-    password: 'waylab',
-    database: 'waylab'
+    host: 'DELETE',
+    user: 'DELETE',
+    password: 'DELETE',
+    database: 'DELETE'
 });
 
 module.exports = dbConnection.promise();
